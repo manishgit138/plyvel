379379be90f5b74e2162f1c0c759f09895acb575
manishgit138 <75783992+manishgit138@users.noreply.github.com>
2021-07-23 17:26:44 +0530

    Update build_wheel_arm64.sh

--- build_wheel_arm64.sh
+++ build_wheel_arm64.sh
@@ -4,7 +4,7 @@ set -ex
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v ${{ github.workspace }}:/ws:rw --workdir=/ws arm64v8/ubuntu:20.04 \
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v ($pwd):/ws:rw --workdir=/ws arm64v8/ubuntu:20.04 \
    bash -exc 'apt-get -y update && apt-get -y install git make curl python3 python3-pip && \
    curl -fsSL https://get.docker.com -o get-docker.sh && \
    sh get-docker.sh && \
