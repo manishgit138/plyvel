7b37e50177d800581944bd56c8cd018962cb3dff
manishgit138 <75783992+manishgit138@users.noreply.github.com>
2021-07-26 19:26:50 +0530

    Update build_wheel_arm64.sh

--- build_wheel_arm64.sh
+++ build_wheel_arm64.sh
@@ -6,9 +6,8 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 pwd
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $pwd:/ws:rw --workdir=/ws arm64v8/ubuntu:20.04 \
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/plyvel:rw --workdir=/plyvel arm64v8/ubuntu:20.04 \
   bash -exc 'ls && \
-  /ws && \
   pwd'
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v $pwd:/plyvel:rw arm64v8/ubuntu:20.04 \
   bash -exc 'apt-get -y update && apt-get -y install git make curl python3 python3-pip && \
